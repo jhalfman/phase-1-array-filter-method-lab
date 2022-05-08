335774756d3543fc7171dc986aa8aeb713a0fbff
Jordan Halfman <jordanhalfman@gmail.com>
2022-05-08 13:27:03 -0700

    commit tree 3fb989c2a782dddc92a9ff299fee53c8be424932

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(names, matcher) {
+   return names.filter(function(name) {return name.toLowerCase() === matcher.toLowerCase()})
+}
+
+function fuzzyMatch(names, letters) {
+    const number = letters.length;
+    return names.filter(function(name) {
+        return name.slice(0, number) === letters;
+    })
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(function(driver) {
+        return driver["name"] === name;
+    })
+}
